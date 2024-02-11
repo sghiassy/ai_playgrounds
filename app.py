ac5708a4ae09f0178e9d206492463d891752b733
Shaheen Ghiassy <sghiassy@linkedin.com>
2024-02-11 09:27:49 -0600

    works

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def img2text(url):
 def generate_story(scenario):
     template = """
     You are a story teller;
-    You can generate a short story based on a simple narrative, the story should be no more than 20 words;
+    You can generate a short story based on a simple narrative, the story should be no more than 50 words;
 
     CONTEXT: {scenario}
     STORY:
